2208430f0b0c7071b6f643cb158cda6e2aedffe2
shiropt <shiro19880610@yahoo.co.jp>
2022-12-05 21:00:07 +0900

    postコントローラ修正

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -12,36 +12,29 @@ import { PostService } from './post.service';
 import { CreatePostResponse } from './response/post.response';
 
 @ApiTags('投稿API')
-@ApiHeader({
-  name: 'X-MyHeader',
-  description: 'Custom header',
-})
 @Controller('post')
 export class PostController {
   constructor(private readonly postService: PostService) {}
 
-  // @Get(':id')
-  // async getPost(@Param('id') id: string): Promise<PostModel | null> {
-  //   return this.postService.getPost(parseInt(id, 10));
-  // }
+  @Get(':id')
+  @ApiOkResponse({ type: PostResponse })
+  async getPost(@Param('id') id: string) {
+    return this.postService.getPost(parseInt(id, 10));
+  }
 
   @Get()
-  async getPosts(): Promise<string> {
-    return 'hello!!';
+  @ApiOkResponse({ type: PostResponse, isArray: true })
+  async getPosts() {
+    return this.postService.getPosts();
   }
 
   @Post()
-  @ApiOkResponse({
-    status: 201,
-    description: 'The record has been successfully created.',
-  })
-  @ApiForbiddenResponse({ status: 403, description: 'Forbidden.' })
   async createPost(@Body() post: CreatePostDto): Promise<PostResponse> {
     return this.postService.createPost(post);
   }
 
-  // @Delete(':id')
-  // async deletePost(@Param('id') id: string): Promise<PostModel> {
-  //   return this.postService.deletePost(parseInt(id, 10));
-  // }
+  @Delete(':id')
+  async deletePost(@Param('id') id: string) {
+    return this.postService.deletePost(parseInt(id, 10));
+  }
 }
